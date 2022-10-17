11ec8a7a782a1f75de13c895e7aa5630086a6e0a
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-10-17 12:09:39 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/edu/infnet/appsegmonpub/MainActivity.kt
@@ -0,0 +1,142 @@
+package br.edu.infnet.appsegmonpub
+
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import br.edu.infnet.appsegmonpub.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.util.Log
+import android.view.WindowManager.InvalidDisplayException
+import androidx.core.app.ActivityCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.webViewClient = WebViewClient()
+
+        binding.webView.loadUrl("https://www.infnet.edu.br/")
+
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (binding.webView.canGoBack())
+            binding.webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+
+
+    fun callLoadImage(view: View) {
+        val iv: ImageView = findViewById(R.id.iv_logo)
+        Picasso.get()
+            .load(
+                "https://www.infnet.edu.br/infnet/wp-content/" +
+                        "uploads/sites/6/2018/01/logotipo.png"
+            )
+            .into(iv)
+    }
+
+    fun callWriteOnExternalStorage(view: View) {
+        // Setar Permissões
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
+                callDialog(
+                    "É preciso liberar WRITE_EXTERNAL_STORAGE",
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_PERMISSIONS_CODE
+                )
+            }
+        } else {
+            createDeleteFile()
+        }
+    }
+
+    // Permissões -  INI
+    private val REQUEST_PERMISSIONS_CODE = 12800
+
+
+    private fun callDialog(message: String, permissions: Array<String>) {
+        var mDialog = AlertDialog.Builder(this)
+            .setTitle("Permissão")
+            .setMessage(message)
+            .setPositiveButton("Ok")
+            { dialog, id ->
+                ActivityCompat.requestPermissions(
+                    this@MainActivity, permissions,
+                    REQUEST_PERMISSIONS_CODE
+                )
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancelar")
+            { dialog, id ->
+                dialog.dismiss()
+            }
+        mDialog.show()
+    }
+
+
+    private fun createDeleteFile() {
+        val file = File(getExternalFilesDir(null), "DemoFile.txt")
+        if (file.exists()) {
+            file.delete()
+        } else {
+            try {
+                val os: OutputStream = FileOutputStream(file)
+                os.write("Pequeno Teste/n Com Quebra de Linha.".toByteArray())
+                os.close()
+            } catch (e: IOException) {
+                Log.d("Permissão", "Erro de escrita em arquivo")
+            }
+        }
+    }
+
+    fun callReadFromExternalStorage(view: View) {
+
+
+        readFile()
+    }
+
+    private fun readFile() {
+
+    }
+
+
+}
